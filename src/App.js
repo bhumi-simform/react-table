142be163386ef7286721919a32882b96db2ca555
Bhumi Lodhari <bhumi.l@simformsolutions.com>
2023-06-19 15:52:39 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import BasicTable from "./componets/basic-table";
+
+function App() {
+  return (
+    <>
+      <BasicTable />
+      <div>This is temporary div</div>
+    </>
+  );
+}
+
+export default App;
